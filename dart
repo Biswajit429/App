53cd101089bf9cd39298f91ea9e9e07df0744529
Biswajit429 <bd2270846@gmail.com>
2026-05-20 17:15:05 +0530

    first day

--- dart
+++
@@ -1,9 +0,0 @@
-1.//write a dart program to print a name and college name .
-
-void main(){
-    string name ="Biswajit Dey";
-    string colllege="ABC college";
-    print("Name: $name");
-    print("College Name: $college");
-    
-}
---
+++ name.dart
@@ -0,0 +1,11 @@
+void main(){
+    String name ="Biswajit Dey";
+    String college="ABC college";
+    int age=20;
+    String Email="Biswajit123@gmail.com";
+    print("Name of the student: $name");
+    print("College Name: $college");
+    print("Age of the student: $age");
+    print("Email:$Email");
+    
+}
